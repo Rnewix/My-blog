26fc9e2ddd50f04884cc6ba8a2264a98149ceb40
Roca Carlos <rocaedono@gmail.com>
2022-09-06 01:06:52 +0900

    making css desing, using bootstrap in forms, finishing about me page

--- My_blog/contact/forms.py
+++ My_blog/contact/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ConctactForm(forms.Form):
-    matter = forms.CharField(max_length = 50, min_length = 1)
-    message = forms.CharField(widget=forms.Textarea)
-    email = forms.EmailField(max_length = 30)
+    matter = forms.CharField(max_length = 50, min_length = 1, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    email = forms.EmailField(max_length = 30, widget=forms.TextInput(attrs={'class': 'form-control'}))
