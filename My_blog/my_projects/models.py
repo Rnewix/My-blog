05b5f88ced1a1022dded127c821d3cb70546545f
Roca Carlos <rocaedono@gmail.com>
2022-09-07 02:21:35 +0900

    Iniciando creacion de modelos para form de Proyects. Cambio de index a un cover, bootstrap

--- My_blog/my_projects/models.py
+++ My_blog/my_projects/models.py
@@ -1,3 +1,19 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+class Articule(models.Model):         
+    id = models.AutoField(primary_key=True, max_length=1000)                                                          
+    name = models.CharField(max_length=25)
+    url= models.URLField()
+    description= models.TextField(null=True, max_length=100)
+    image=models.ImageField(upload_to='imagenes/', null=True, blank=True)
+
+    def __str__(self):
+        showname= self.name + ' -- ' + self.description  
+        return showname    
+    
+#    def delete(self, using=None, keep_parents=False):
+#        self.image.storage.delete(self.image.id)
+#        super().delete()
